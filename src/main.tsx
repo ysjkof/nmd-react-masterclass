da266dddc28e3dd57d5610ea51890553b224c658
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-05-13 10:55:55 +0900

    #4 CRTPTO TRACKER

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
