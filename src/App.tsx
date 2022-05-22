932d1f3b3f76a2fac654eb79c2adefa16a85583f
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-05-22 19:01:41 +0900

    #5 STATE MANAGEMENT - React Hook Form
    #5.6 ~ #5.9

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
@@ -68,6 +69,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
