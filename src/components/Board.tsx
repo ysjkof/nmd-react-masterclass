f114b519158eb73d9eaabc9477ce1fd75128ef37
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-05-23 13:25:11 +0900

    #6 TRELLO CLONE - Finish

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,8 @@
 import { Droppable } from "react-beautiful-dnd";
+import { useForm } from "react-hook-form";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
+import { ITodo, toDoState } from "../atoms";
 import DragabbleCard from "./DragabbleCard";
 
 const Wrapper = styled.div`
@@ -37,26 +40,66 @@ const Area = styled.div<IAreaProps>`
   padding: 20px;
 `;
 
+const Form = styled.form`
+  width: 100%;
+  input {
+    width: 100%;
+  }
+`;
+
 interface IBoardProps {
-  toDos: string[];
+  toDos: ITodo[];
   boardId: string;
 }
 
+interface IForm {
+  toDo: string;
+}
+
 function Board({ toDos, boardId }: IBoardProps) {
+  const setToDos = useSetRecoilState(toDoState);
+  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const onValid = ({ toDo }: IForm) => {
+    const newToDo = {
+      id: Date.now(),
+      text: toDo,
+    };
+    setToDos((allBoards) => {
+      return {
+        ...allBoards,
+        [boardId]: [newToDo, ...allBoards[boardId]],
+      };
+    });
+    setValue("toDo", "");
+  };
   return (
     <Wrapper>
       <Title>{boardId}</Title>
+      <Form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("toDo", { required: true })}
+          type="text"
+          placeholder={`Add task on ${boardId}`}
+        />
+      </Form>
       <Droppable droppableId={boardId}>
         {(magic, info) => (
           <Area
             isDraggingOver={info.isDraggingOver}
             isDraggingFromThis={Boolean(info.draggingFromThisWith)}
+            // ref = react코드로 HTML요소를 지정하고 가져오는 방법. react js component를 통해 html 요소를 가져오게 한다.
+            // useRef()는 React에서 document.querySelector() 등을 대체하는 기능
             ref={magic.innerRef}
             {...magic.droppableProps}
           >
             {toDos.map((toDo, index) => (
               // 여기서 key에 map의 index를 주면 안된다. 일반적으로 draggableId를 key에 사용한다.
-              <DragabbleCard key={toDo} index={index} toDo={toDo} />
+              <DragabbleCard
+                key={toDo.id}
+                index={index}
+                toDoId={toDo.id}
+                toDoText={toDo.text}
+              />
             ))}
             {/* Draggable을 드래그할 때 Droppable가 작아지는 것을 방지함. Draggable의 형제로 렌더링한다 */}
             {magic.placeholder}
