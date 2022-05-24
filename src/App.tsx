6990e79dc91adeb56fa6bc0a551f9e90bff75daa
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-05-24 10:31:41 +0900

    #8 NOMFLIX CLONE - Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
